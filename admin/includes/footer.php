d69b29962f975a38408e6363f6c50d64e7e4a9e0
pinnintikusuma <37824069+pinnintikusuma@users.noreply.github.com>
2018-03-27 10:09:38 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2018 ANITS Online Library Management System |<a href="http://www.anits.edu.in" target="_blank"  > Designed by : Batch No: 9</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
